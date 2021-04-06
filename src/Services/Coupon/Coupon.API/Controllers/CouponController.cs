20a50884c54de0648cad7fcdd7d8023f7c7e9daa
Sahil Joseph <sahiljoseph20@gmail.com>
2021-04-06 19:07:32 +0530

    Update CouponController.cs

--- src/Services/Coupon/Coupon.API/Controllers/CouponController.cs
+++ src/Services/Coupon/Coupon.API/Controllers/CouponController.cs
@@ -53,8 +53,9 @@
                 return NotFound(coupon == null ? "ERROR: The coupon doesn't exist" : "ERROR: The coupon has been redeemed already");
 
             var couponDto = _mapper.Translate(coupon);
-
-            // Add LogInformation call
+            
+            _logger.LogInformation("Applying coupon {CouponCode}", code);
+             
 
             return couponDto;
         }
